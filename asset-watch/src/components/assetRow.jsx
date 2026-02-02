90f5dfda2be8663a0fd8a35a0c645061ef02e80f
Becky Kalumu <beckykalumu8@gmail.com>
2026-02-02 10:23:06 +0300

    Refactor AssetDetail to AssetRow component

--- asset-watch/src/components/assetRow.jsx
+++ asset-watch/src/components/assetRow.jsx
@@ -1,15 +1,23 @@
-import { useParams } from "react-router-dom";
-import PriceChart from "../components/PriceChart";
+import React from 'react';
+import PriceChart from './PriceChart';
+import "./assetRow.css";
 
-const AssetDetail = () => {
-  const { id } = useParams();
-
+const AssetRow = ({ asset }) => {
   return (
-    <div>
-      <h1>Asset: {id}</h1>
-      <PriceChart assetId={id} />
+    <div className="asset-row">
+      <div className="asset-info">
+        <span className="asset-name">{asset.name}</span>
+        <span className="asset-symbol">({asset.symbol})</span>
+        <span className="asset-price">${asset.current_price.toFixed(2)}</span>
+        <span className={`asset-change ${asset.change >= 0 ? 'positive' : 'negative'}`}>
+          {asset.change >= 0 ? '+' : ''}{asset.change}%
+        </span>
+      </div>
+      <div className="asset-chart">
+        <PriceChart sparkline="{asset.sparkline}" />
+      </div>
     </div>
   );
-};
+}
 
-export default AssetDetail;
+export default AssetRow;
